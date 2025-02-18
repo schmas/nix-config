3e790cee7ce0bdc4c4bb3c5631f0044c2ddac65f
Diego Schmidt <schmas+gh@pm.me>
2025-02-18 10:42:07 -0300

    updated

--- modules/darwin/homebrew-packages.nix
+++ modules/darwin/homebrew-packages.nix
@@ -3,22 +3,70 @@
     enable = true;
     brews = [ "urlview" "mise" "openssl" ];
     casks = [
+      # Security & Privacy
       "1password"
+      "gpg-suite-no-mail"
+      "proton-pass"
+      "protonvpn"
+      "tunnelblick"
+      "yubico-yubikey-manager"
+
+      # Productivity & Utilities
+      "alfred"
+      "alt-tab"
       "bartender"
+      "cleanmymac"
       "cleanshot"
       "doll"
-      "ghostty"
-      "google-chrome"
-      "gpg-suite"
+      "hammerspoon"
+      "imageoptim"
+      "keepingyouawake"
       "leader-key"
+      "logi-options+"
       "monitorcontrol"
+      "neohtop"
+      "notion"
+      "notion-calendar"
       "pixelsnap"
       "rectangle-pro"
-      "slack"
-      "textmate"
+      "transnomino"
+
+      # Development Tools
+      "ghostty"
+      "jetbrains-toolbox"
+      "podman-desktop"
+      "postman"
       "visual-studio-code"
-      "vivaldi"
       "zed"
+
+      # Browsers & Communication
+      "discord"
+      "google-chrome"
+      "slack"
+      "vivaldi"
+      "zoom"
+
+      # Cloud Storage & Sync
+      "google-drive"
+      "proton-drive"
+      "proton-mail"
+
+      # Media & Entertainment
+      "spotify"
+      "steam"
+      "whisky"
+      "heroic"
+
+      # Design & Creative
+      # "figma"
+      # "godot"
+
+      # Virtual Machines & Remote Access
+      # "rustdesk"
+      "utm"
+
+      # Microsoft
+      # "microsoft-auto-update"
     ];
     onActivation = {
       #cleanup = "zap";
