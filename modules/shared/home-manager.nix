e9bbb0929eff17a1703406d116ca7531ad201058
Diego Schmidt <schmas+gh@pm.me>
2025-02-18 21:25:38 -0300

    updated

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -92,10 +92,11 @@ in
 
       interactiveShellInit = ''
         # Your custom bash code goes here
-        if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
-          . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-          . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
-        fi
+        # Nix setup
+        if test -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+            source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
+            source /nix/var/nix/profiles/default/etc/profile.d/nix.fish
+        end
 
         ##############################
         # DOTFILES
