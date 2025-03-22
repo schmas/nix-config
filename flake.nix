4e76a6a5eb0969d1e222aa2403060f52c846aa2c
Diego Schmidt <schmas+gh@pm.me>
2025-03-22 13:52:17 -0300

    feat: update flake.lock and flake.nix for nix-homebrew repository changes

--- flake.nix
+++ flake.nix
@@ -23,13 +23,8 @@
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-homebrew = {
-      # url = "github:zhaofengli-wip/nix-homebrew";
-      # temporary fix, remove when merged
-      url = "git+https://github.com/zhaofengli/nix-homebrew?ref=refs/pull/71/merge";
-    };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
     mac-app-util.url = "github:hraban/mac-app-util";
-
   };
 
   outputs =
